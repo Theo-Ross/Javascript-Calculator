2e4d103a10cc1000ae37615c5f62e27ac5ddc396
Theo-Ross <theoross99@icloud.com>
2022-11-23 15:12:29 +0000

    new display

--- script2.js
+++ script2.js
@@ -1,11 +1,8 @@
-const buttons = Array.from(document.querySelectorAll(".numbers"))
+const buttons = Array.from(document.querySelectorAll(".numbers"));
 
 const buttonC = document.querySelector(".operator__grey-C");
 const buttonSymbol = document.querySelector(".operator__grey-symbol");
 const buttonPercent = document.querySelector(".operator__grey-percent");
-
-
-
 
 let buttonDivide = document.querySelector(".operator__divide");
 let buttonX = document.querySelector(".operator__x");
@@ -13,41 +10,38 @@ let buttonMinus = document.querySelector(".operator__minus");
 let buttonPlus = document.querySelector(".operator__plus");
 let buttonEquals = document.querySelector(".operator__equals");
 
-let display = document.querySelector(".display")
-
-
+let display = document.querySelector(".display");
+let display2 = document.querySelector(".display2");
 let number = "";
 let symbol = "";
 
 //buttons
 
 buttons.forEach((event) => {
-    event.addEventListener("click", () => {
-      return display.innerHTML += event.innerHTML;
-    })
-    console.log(display.innerHTML)
-  })
-  
-
+  event.addEventListener("click", () => {
+    return (display.innerHTML += event.innerHTML);
+  });
+  console.log(display.innerHTML);
+});
 
 // function buttons
 
 buttonC.addEventListener("click", () => {
-    display.innerHTML = "";
-    number = "";
-  });
+  display.innerHTML = "";
+  number = "";
+  display2.innerHTML = "";
+});
 
-
-// operator buttons 
-  
+// operator buttons
 
 const makeSum = (event) => {
-    number = Number(display.innerHTML);
-    console.log(number)
-    symbol = event.target.innerHTML;
-    console.log(symbol);
-    display.innerHTML = ""
-}
+  number = Number(display.innerHTML);
+
+  symbol = event.target.innerHTML;
+
+  display2.innerHTML = number + symbol;
+  display.innerHTML = "";
+};
 
 buttonDivide.addEventListener("click", makeSum);
 
@@ -57,54 +51,49 @@ buttonMinus.addEventListener("click", makeSum);
 
 buttonX.addEventListener("click", makeSum);
 
-
-
 // percentage function
 
 const makePercent = (event) => {
-    number = Number(display.innerHTML);
-    number = number / 100;
-    display.innerHTML = number;
-}
+  number = Number(display.innerHTML);
+  number = number / 100;
+  display.innerHTML = number;
+};
 
-buttonPercent.addEventListener("click", makePercent)
+buttonPercent.addEventListener("click", makePercent);
 
-
-// positive - negative function 
-
+// positive <--> negative function
 
 const changeValue = (event) => {
-     number = Number(display.innerHTML);
-    if (display.innerHTML.includes("-")) { 
-        number = Math.abs(number);
-        display.innerHTML = number;
-    } else {
-        number = -Math.abs(number);
-        display.innerHTML = number;
-    }
-   
-}
+  number = Number(display.innerHTML);
+  if (display.innerHTML.includes("-")) {
+    number = Math.abs(number);
+    display.innerHTML = number;
+  } else {
+    number = -Math.abs(number);
+    display.innerHTML = number;
+  }
+};
 
-buttonSymbol.addEventListener("click", changeValue)
+buttonSymbol.addEventListener("click", changeValue);
 
+// equals button
 
-  // equals button
-
-  const makeAnswer = () => {
-    if (symbol === "X") {
-        number *= Number(display.innerHTML)
-        display.innerHTML = number;
-    } else if (symbol === "+") {
-        number += Number(display.innerHTML)
-        display.innerHTML = number;
-    } else if (symbol === "-") {
-        number -= Number(display.innerHTML)
-        display.innerHTML = number;
-    } else if (symbol === "÷") {
-        number /= Number(display.innerHTML)
-        display.innerHTML = number;
-    } else {
-        display.innerHTML = "ERROR"
-    }
-}
+const makeAnswer = () => {
+  display2.innerHTML = "";
+  if (symbol === "X") {
+    number *= Number(display.innerHTML);
+    display.innerHTML = number;
+  } else if (symbol === "+") {
+    number += Number(display.innerHTML);
+    display.innerHTML = number;
+  } else if (symbol === "-") {
+    number -= Number(display.innerHTML);
+    display.innerHTML = number;
+  } else if (symbol === "÷") {
+    number /= Number(display.innerHTML);
+    display.innerHTML = number;
+  } else {
+    display.innerHTML = "ERROR";
+  }
+};
 buttonEquals.addEventListener("click", makeAnswer);
